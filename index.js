0bd08f1d207f57d4418604b4cc92ab1712b528d4
Vishnu <vvichugit@gmail.com>
2019-07-31 15:42:00 -0500

    added the node class

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 //Hey this is the code for Linkedlist: 
 //Hit the terminal and run node FileName without .js eg: FileName: index.js run in the terminal node index. 
+
+class Node{
+    //A node has a data and a reference to that of the next node. 
+    constructor(data, next = null){
+      this.data = data;
+      this.next = next;
+    }
+
+}
+
+const n1 = new Node(100); 
+
+console.log(n1);
